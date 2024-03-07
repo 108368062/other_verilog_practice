1ca3b949494503b5aaa121f4a2652b67b06e3034
tingwei <125willie27e@gmail.com>
2024-03-07 23:38:57 +0800

    update success verion

--- 3bit_lut/bit_3_lut.v
+++ 3bit_lut/bit_3_lut.v
@@ -18,14 +18,14 @@ module top_module (
     end
     always@(*) begin
         case({A,B,C})
-            3'h000:Z=Q[0];
-            3'h001:Z=Q[1];
-            3'h010:Z=Q[2];
-            3'h011:Z=Q[3];
-            3'h100:Z=Q[4];
-            3'h101:Z=Q[5];
-            3'h110:Z=Q[6];
-            3'h111:Z=Q[7];
+            3'h0:Z=Q[0];
+            3'h1:Z=Q[1];
+            3'h2:Z=Q[2];
+            3'h3:Z=Q[3];
+            3'h4:Z=Q[4];
+            3'h5:Z=Q[5];
+            3'h6:Z=Q[6];
+            3'h7:Z=Q[7];
         endcase
     end
         
